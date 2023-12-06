c5501477dc3a3dbda3468006fb3c0a8c31d7cd45
sd00 <bolobs91@gmail.com>
2023-12-06 18:17:04 +0100

    style: add display flex and center div

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function createGrid() {
-    let size = 16;
+    let size = document.querySelector('#myRange').value;
     let grid = document.querySelector('#grid');
     
 
     for (let i = 0; i < (size*size); i++) {
         let square = document.createElement('div');
         let squareSize = document.querySelector('#grid').clientWidth / size;
-        square.style.width = squareSize + 'px';
-        square.style.height = squareSize + 'px';
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
         square.classList.add ('gridSquare');
         grid.appendChild(square);
     }
